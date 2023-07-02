8b2181541e2350c349b70a508d90e7637e8149ee
PusparghyaMukherjee1510 <pusparghyamukherjee10@gmail.com>
2023-07-02 11:15:49 +0530

    updated

---
+++ arrayer.py
@@ -0,0 +1,18 @@
+from array import *
+arr=array('i',[1,2,3,4,5])
+# print(type(arr))
+# print(arr[0])
+# print(arr[2])
+
+#for loop in array to print elements
+#for j in arr:
+    #print(j,end=' ')
+    # try:
+    #  print(arr[j])
+    # except:
+    #    #print('error handled')
+    #    print(arr[j])
+
+#pointer for loop 
+#for pnt in range(5):
+    #print(arr[pnt],end=" ")
