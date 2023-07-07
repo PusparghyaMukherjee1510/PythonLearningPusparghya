f1865434f12e2faffa5a8c30b85605e31aeae13c
PusparghyaMukherjee1510 <pusparghyamukherjee10@gmail.com>
2023-07-07 19:37:57 +0530

    git commit --amend

--- funtioner.py
+++ funtioner.py
@@ -1,4 +1,15 @@
 #first example 
-def welcome():
-    print('GM')
-welcome()
+# def welcome():
+#     print('GM')
+# welcome()
+
+#passing arguments
+# def add(a,b):
+#     #print(a+b)
+#     tot=a+b
+#     print('the sum is', tot)
+# add(10,20)
+
+#def asd(a,c):
+ #   print(a+c)
+#asd(int(input('enter first argument ')),int(input('enter second argument ')))
