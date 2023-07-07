8b54c1bf102f9c2d8553055e4713430feba9ccc6
PusparghyaMukherjee1510 <pusparghyamukherjee10@gmail.com>
2023-07-07 19:50:15 +0530

    git commit --amend

--- funtioner.py
+++ funtioner.py
@@ -13,3 +13,11 @@
 #def asd(a,c):
  #   print(a+c)
 #asd(int(input('enter first argument ')),int(input('enter second argument ')))
+
+#sum of 4 nos
+# def add(*a):
+#     tot=0
+#     for i in a:
+#         tot+=i
+#     print("sum is", tot)
+# add(10,20,30,40)
