a8f69e82e44e0910005eec611c5b7b29f46959df
PusparghyaMukherjee1510 <pusparghyamukherjee10@gmail.com>
2023-07-13 20:12:10 +0530

    git commit --amend

---
+++ obj&cls.py
@@ -0,0 +1,56 @@
+# class Person():
+#     def __init__(self) -> None:
+#          self.name="Sam"
+#          self.age=12
+#          self.gender="Male"
+#     def talk(self):
+#          print("Hi, I\'m",self.name)
+#     def vote(self):
+#          if self.age<18:
+#               print("I\'m unable to  Vote")
+#          else :
+#               print("I\'m able to Vote")
+
+# obg=Person()
+#Person.talk(obg)
+#Person.vote(obg)
+#different way to call class methods
+# obg.talk()
+# obg.vote()
+
+
+#create multiple objects
+# class Person():
+#     def __init__(self,n,g,a) -> None:
+#          self.name=n
+#          self.age=a
+#          self.gender=g
+#     def talk(self):
+#          print("Hi, I\'m",self.name)
+#     def vote(self):
+#          if self.age<18:
+#               print("I\'m unable to  Vote")
+#          else :
+#               print("I\'m able to Vote")
+
+# ojn=Person("DOP","Male",98)
+# ojm=Person("Jass","Female",96)
+# ojn.talk()
+# ojn.vote()
+# ojm.talk()
+# ojm.vote()
+
+# class Car():
+#     def getSpeed(self,n,s):
+#         self.name=n
+#         self.speed=s
+#         print("Name is ",self.name)
+#         print("Speed is ",self.speed)
+
+# ofd=Car()
+# ofg=Car()
+# ofd.getSpeed("BMW",120)
+# ofg.getSpeed("TATA",80)
+# Toy=Car()
+# Car.getSpeed(Toy,"Toyota",78)
+
