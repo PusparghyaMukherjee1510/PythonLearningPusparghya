09cfeba5a736518bec16115e28d6f4cba9442721
PusparghyaMukherjee1510 <pusparghyamukherjee10@gmail.com>
2023-07-16 14:19:03 +0530

    git commit --amend

--- libo.py
+++ libo.py
@@ -78,3 +78,55 @@ import time
 # s1=pd.Series(g,index=ind)
 # print(s1)
 
+#Series Op
+# arr=[0,1,2]
+# arr1=[4,5,6]
+# s5=pd.Series(arr)
+# s6=pd.Series(arr1)
+# print(s5)
+# print(s6)
+# print(s5.add(s6))
+# print(s5.median())
+# print(s6.median())
+# print(s5.max())
+# print(s6.max())
+# print(s5.min())
+# print(s6.min())
+
+#Create DataFrame
+#dts=pd.date_range('today',periods=7)
+#print(dts)
+# nm_arr=np.random.randn(7,4)
+# clms=['A','B','C','D']
+# df1=pd.DataFrame(nm_arr,index=dts,columns=clms)
+# print(df1)
+
+datas={'animal':['cat','cat','dog','dog','tiger','tiger','dog','tiger','lion','dog'],
+      'age':[2.5,3,2,3.3,2,2,5.4,5,6.2,6.1],
+      'visits':[1,1,2,2,3,4,5,6,3,2],
+      'priority':['yes','yes','yes','yes','no','no','no','no','yes','no']}
+
+labels=['a','b','c','d','e','f','g','h','i','j']
+
+df2=pd.DataFrame(datas,index=labels)
+#print(df2)
+#print(df2.describe())
+#print(df2.T)
+#print(df2.sort_values(by='age'))
+#print(df2[1:3])
+#print(df2[['age','visits']])
+df3=df2.copy()
+# print(df3.isnull())
+df3.loc['f','age']=9
+#print(df3)
+#print(df3.mean())
+#print(df3['visits'].mean())
+#print(df3.sum())
+# strng=pd.Series(['a','b','Lop',23,9.76,np.nan])
+# print(strng.str.upper())
+# print(strng.str.lower())
+# print(df2)
+# print("\n\n")
+# print(df3)
+#df3.to_csv('animaldf3.csv')
+#df3.to_excel('animaldf3.xlsx')
