68c9a5fd78a586654de4b3ba5a60a3e6c862227c
PusparghyaMukherjee1510 <pusparghyamukherjee10@gmail.com>
2023-08-24 13:25:35 +0530

    git commit -amend

---
+++ sts.py
@@ -0,0 +1,21 @@
+# tpl1=([2,3],"bnm",5,7)
+# tpl1[0][1]=1
+# print(tpl1)
+
+# str4="7/5"
+# print(str4)
+
+# a={3:"X",2:"B",7:"C"}
+# #print(type(a))
+# b=a.copy() 
+# b[2]="D"
+# print(a)
+
+#list1=[4,9,5,7,3,2] 
+# list1.pop(3) 
+# print(list1)
+#print(list1[:-1])
+
+list1=["john","arya","simon"] 
+list1.insert(5,8) 
+print(list1)
